ab9d505297ced0bc0f7cfc71cecbed14f9e22da9
Lobzev Nikita <lobzev.n@yandex.ru>
2025-03-30 02:24:52 +0300

    Все работает, остался небольшой рефакторинг дополнительный

---
+++ data/src/main/java/com/example/data/firstusage/AppPreferences.kt
@@ -0,0 +1,26 @@
+package com.example.data.firstusage
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class AppPreferences(private val context: Context) {
+    val sharedPrefs: SharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+
+    companion object {
+        private const val KEY_FIRST_LAUNCH = "is_first_launch"
+    }
+
+    fun isFirstLaunch(): Boolean {
+        return sharedPrefs.getBoolean(KEY_FIRST_LAUNCH, true)
+    }
+
+    fun setLaunched() {
+        sharedPrefs.edit().putBoolean(KEY_FIRST_LAUNCH, false).apply()
+    }
+
+    fun clearPreferences() {
+        sharedPrefs.edit().clear().apply()
+    }
+
+
+}
